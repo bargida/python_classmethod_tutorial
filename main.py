6ed22abe65064d90c054b5f4b0e7d51240fee9e5
bargida <bargida.tilyakova25@gmail.com>
2026-02-07 20:56:29 +0500

    updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 # Definition of class 
 class DataTable:
@@ -10,23 +11,23 @@ class DataTable:
         self.price = []
         self.load_data()
 
-
     def load_data(self):
         with open(self.data_path, 'r') as file:
-            data = json.load(file)
-        self.item = [1,2,3]
-        self.category = [2,3,4]
-        self.price = [4,5,6]
-
-        return data
-
-
-
-        
-
-
+            # data = json.load(file)
+            data = csv.reader(file)
+        # self.item = [1,2,3]
+        # self.category = [2,3,4]
+        # self.price = [4,5,6]
+            for items in data:
+                # print(items)
+                if len(items) >= 3:
+                    self.item.append(items[0])
+                    self.category.append(items[1])
+                    self.price.append(items[2]) 
+            
 # Create DataTable object
-data_path = 'grocery_list.json'
+# data_path = 'grocery_list.json'
+data_path = 'grocery_list.csv'
 table = DataTable(data_path)
 
 print(table.item)
